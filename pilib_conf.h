871650d459d0b8a42c1b8fe764ba27c13b075576
Pururun26 <pururunkun26@gmail.com>
2026-08-26 16:28:23 +0300

    Create pilib_conf.h

---
+++ pilib_conf.h
@@ -0,0 +1,9 @@
+#ifndef PILIB_CONF_H
+#define PILIB_CONF_H
+
+// Настройки по умолчанию (пользователь может отредактировать)
+#define PILIB_WIDTH   128
+#define PILIB_HEIGHT  128
+#define PILIB_TITLE   "pilib game"
+
+#endif

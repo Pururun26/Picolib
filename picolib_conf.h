ab86d44b84ec7641e6a51a4aabf845b0e942b8b9
Pururun26 <pururunkun26@gmail.com>
2026-08-26 19:04:18 +0300

    Update and rename pilib_conf.h to picolib_conf.h

--- picolib_conf.h
+++ picolib_conf.h
@@ -1,12 +1,12 @@
-#ifndef PILIB_CONF_H
-#define PILIB_CONF_H
+#ifndef PICOLIB_CONF_H
+#define PICOLIB_CONF_H
 
 // Настройки по умолчанию (пользователь может отредактировать)
-#define PILIB_WIDTH   128
-#define PILIB_HEIGHT  128
-#define PILIB_TITLE   "pilib game"
+#define PICOLIB_WIDTH   128
+#define PICOLIB_HEIGHT  128
+#define PICOLIB_TITLE   "picolib game"
 
 // Путь и имя файла спрайт-листа (должен быть 128x128 пикселей для 256 спрайтов 8x8)
-#define PILIB_SS      "pilib_spritesheet.png"
+#define PICOLIB_SS      "picolib_spritesheet.png"
 
 #endif

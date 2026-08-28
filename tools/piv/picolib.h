70460ee59666606aa556dfcd6ad65e085a69027e
Pururun26 <pururunkun26@gmail.com>
2026-08-28 18:12:55 +0300

    Create picolib.h

---
+++ tools/piv/picolib.h
@@ -0,0 +1,55 @@
+#ifndef PICOLIB_H
+#define PICOLIB_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "picolib_conf.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Простая структура для возврата координат (аналог tuple в Lua)
+typedef struct {
+    int16_t x;
+    int16_t y;
+} picolib_vec2;
+
+// --- Инициализация ---
+void picolib_load_spritesheet(const char* filepath); // Загрузка спрайт-листа
+void picolib_load_font(const char* filepath);
+
+// --- Основные функции ---
+void update(void);
+void draw(void);
+
+// --- API для рисования ---
+void cls(uint8_t color);
+void print(const char *text, int16_t x, int16_t y, uint8_t color);
+void print_pro(const char* format, int16_t x, int16_t y, uint8_t color, ...);
+void circ(int16_t x, int16_t y, int16_t r, uint8_t color);
+void circfill(int16_t x, int16_t y, int16_t r, uint8_t color);
+void rect(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint8_t color);
+void rectfill(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint8_t color);
+
+// --- Спрайты ---
+// Простая версия: рисует один спрайт 8x8
+void spr(int16_t n, int16_t x, int16_t y);
+
+// Полная версия: w и h теперь указывают КОЛИЧЕСТВО блоков 8x8 (а не пиксели!)
+void spr_pro(int16_t n, int16_t x, int16_t y, uint8_t w, uint8_t h, bool flip_x, bool flip_y);
+
+// --- API для ввода ---
+bool btn(uint8_t id);
+bool btnp(uint8_t id);
+
+// --- API для камеры ---
+// Устанавливает смещение камеры и возвращает предыдущее значение.
+// Чтобы сбросить камеру, вызовите camera(0, 0).
+picolib_vec2 camera(int16_t x, int16_t y);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

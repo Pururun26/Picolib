8a4c8ab065c5569f23daf5e2f5c83c24fb93e8fb
Pururun26 <pururunkun26@gmail.com>
2026-08-28 19:34:09 +0300

    Update picolib_conf.h

--- tools/piv/picolib_conf.h
+++ tools/piv/picolib_conf.h
@@ -5,6 +5,7 @@
 #define PICOLIB_WIDTH   128
 #define PICOLIB_HEIGHT  128
 #define PICOLIB_TITLE   "PIV"
+#define PICOLIB_FPS     60
 
 // Путь и имя файла спрайт-листа (должен быть 128x128 пикселей для 256 спрайтов 8x8)
 #define PICOLIB_SS      "picolib_spritesheet.png"
